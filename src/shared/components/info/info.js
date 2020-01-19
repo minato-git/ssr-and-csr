96bb4b2a4bd25c5f2737439337ef808845e72032
= <=>
2020-01-19 17:13:06 +0530

    pdp page

--- src/shared/components/info/info.js
+++ src/shared/components/info/info.js
@@ -2,7 +2,26 @@ import React from 'react';
 import './info.css';
 
 const Info = ({ info }) => {
-  return <div className={'pdp-info'}>Info</div>;
+  return (
+    <div className={'pdp-info'}>
+      <div className={'key-data'}>
+        <label className={'tag-name'}>Product Name</label>
+        <span className={'tag-info'}>{info.name}</span>
+      </div>
+      <div className={'key-data'}>
+        <label className={'tag-name'}>Rating</label>
+        <span className={'tag-info'}>{info.rating}</span>
+      </div>
+      <div className={'key-data'}>
+        <label className={'tag-name'}>Price</label>
+        <span className={'tag-info'}>{info.price}</span>
+      </div>
+      <div className={'key-data'}>
+        <label className={'tag-name'}>Description</label>
+        <span className={'tag-info desc'}>{info.description}</span>
+      </div>
+    </div>
+  );
 };
 
 export default Info;
---
+++ src/shared/components/ratingStar/ratingStar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const RatingStar = ({rating}) => {
+    const remainig = 5 - rating;
+
+    const stars = () => {
+        let html = [];
+        for(let i = 0; i< count; i++) {
+            <div id={'star'}>
+
+            </div>
+        }
+    }
+
+    return
+    (
+
+    )
+};
+
+export default RatingStar;
